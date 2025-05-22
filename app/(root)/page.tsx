14546221cf8d4d675965e0b0dd63c8bcb55a5fb3
Fabian <fgraeml@yahoo.de>
2025-05-22 13:21:52 +0200

    Assets & Metadata

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,8 @@ import Hello from "@/app/components/hello";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl">Welcome to Next.js</h1>
+      <h1 className="h1-bold">Welcome to Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Next.js</h1>
       <Hello />
     </>
   );
