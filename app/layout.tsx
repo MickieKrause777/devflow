2b289166b840743c4eed1584bf3dc2fadd5a8e7d
Fabian <fgraeml@yahoo.de>
2025-05-22 16:59:12 +0200

    Light & Dark Theme, Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
+import { ThemeProvider } from "next-themes";
+
+import Navbar from "@/components/navigation";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -29,11 +32,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
