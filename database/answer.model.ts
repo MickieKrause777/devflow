996d5fc539f4007bfe1a083c97bf1cac4cf95a50
Fabian <fgraeml@yahoo.de>
2025-08-28 17:34:21 +0200

    Implement answer, question, tag, tag-question model

---
+++ database/answer.model.ts
@@ -0,0 +1,25 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IAnswer {
+  content: string;
+  question: Types.ObjectId;
+  author: Types.ObjectId;
+  createdAt: Date;
+  upvotes: number;
+  downvotes: number;
+}
+
+const AnswerSchema = new Schema<IAnswer>(
+  {
+    content: { type: String, required: true },
+    question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+const Answer = models?.Answer || model<IAnswer>("Answer", AnswerSchema);
+
+export default Answer;
