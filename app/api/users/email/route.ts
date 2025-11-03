760791e3929e117ce9f371f6854543fd39f02a96
Fabian <fgraeml@yahoo.de>
2025-11-03 12:49:57 +0100

    Implement user by id and email

---
+++ app/api/users/email/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { email } = body;
+
+  try {
+    await dbConnect();
+
+    const validatedData = UserSchema.pick({ email }).safeParse(body);
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) throw new NotFoundError("User");
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
