8907651a7037720fc038c24ea1fb8ed625e27a16
Fabian <fgraeml@yahoo.de>
2026-01-01 15:51:49 +0100

    Display All Answers

--- constants/states.ts
+++ constants/states.ts
@@ -38,6 +38,11 @@ export const EMPTY_TAGS = {
   },
 };
 
+export const EMPTY_ANSWERS = {
+  title: "No Answers Found",
+  message: "The answer board is empty. Add some keywords to make it rain.",
+};
+
 export const EMPTY_COLLECTIONS = {
   title: "Collections Are Empty",
   message:
